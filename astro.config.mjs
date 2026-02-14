ebff67c9bdcb169736d8083ad4ac72d2d52de5f7
HainingCui <92837364+HainingCui@users.noreply.github.com>
2026-02-14 10:20:20 +0900

    Update astro.config.mjs
    change for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(),mdx()],
-  srcDir: './website'
+  integrations: [tailwind(), react(), mdx()],
+  srcDir: "./website",
+  site: "https://ncclients.github.io",
+  base: "/qpotsanjose/",
 });
