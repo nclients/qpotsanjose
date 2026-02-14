70a263de19a51a0fa3a332c27c8b064226fcb7f3
HainingCui <92837364+HainingCui@users.noreply.github.com>
2026-02-14 10:27:26 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   srcDir: "./website",
-  site: "https://ncclients.github.io",
-  base: "/qpotsanjose/",
+  // site: "https://ncclients.github.io",
+  // base: "/qpotsanjose/",
 });
